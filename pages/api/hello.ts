520b9266fa67af9ea815b7a9b29f87f92497864a
Thalysson Leite <thalyssonl17@gmail.com>
2022-10-17 11:12:57 -0300

    adding edge computing to project

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -6,20 +6,19 @@ export default async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
-    console.log('Passei aqui 1')
     try {
     const jwLink: any = req.query.link || '';
     const jwURL = 'https://www.jw.org/'+decodeURIComponent(jwLink);
 
-    console.log('Passei aqui 2')
+    const jwHTML = await axios.get(jwURL).then(response => response.data);
 
-    // const jwHTML = await axios.get(jwURL).then(response => response.data);
-
-    console.log('Passei aqui 3')
-
-    res.status(200).json({html: 'p', url: jwURL});
+    res.status(200).json({html: jwHTML, url: jwURL});
   } catch (error) {
     console.log(error)
     res.status(404).end(null);
   }
+};
+
+export const config = {
+  runtime: 'experimental-edge'
 };
